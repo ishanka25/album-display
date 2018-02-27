ce1f827cab3f82a0da7a4c7ccdadfa6f20359e93
ishanka25 <ishan.ranatunga@gmail.com>
2018-02-27 21:21:36 +0530

    init + Header comp

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+//component
+const Header = () => {
+    return <Text>Albums</Text>;
+};
+
+//make availabe to other part of the app
+export default Header;
